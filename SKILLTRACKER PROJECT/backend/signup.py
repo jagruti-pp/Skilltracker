72171199b4f52a6b77acd92aa369beb489dcde30
jagruti-pp <jagrutithotam13@gmail.com>
2025-06-25 22:30:16 +0530

    Update signup.py

--- SKILLTRACKER PROJECT/backend/signup.py
+++ SKILLTRACKER PROJECT/backend/signup.py
@@ -2,7 +2,11 @@ from flask import Flask, request, jsonify
 import psycopg2
 from flask_cors import CORS
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+import psycopg2
 
+load_dotenv()
 print("✅ Starting script...")
 
 app = Flask(__name__)
@@ -12,11 +16,11 @@ print("✅ Flask app created")
 
 try:
     conn = psycopg2.connect(
-        dbname="skillltracker",
-        user="postgres",
-        password="samiksha",
-        host="localhost",
-        port="5432"
+         dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
     )
     cursor = conn.cursor()
     print("✅ Connected to PostgreSQL")
